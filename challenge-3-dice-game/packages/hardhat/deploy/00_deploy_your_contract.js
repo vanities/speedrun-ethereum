bd9f1bc1c290eddcb719330e621ed1495626400f
Adamant Mischke <mischkeaa@gmail.com>
2022-07-03 21:45:15 -0500

    add challenge 3, dice game

---
+++ challenge-3-dice-game/packages/hardhat/deploy/00_deploy_your_contract.js
@@ -0,0 +1,23 @@
+
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  await deploy("DiceGame", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    value: ethers.utils.parseEther(".05"),
+    log: true,
+  });
+
+  // Getting a previously deployed contract
+  const DiceGame = await ethers.getContract("DiceGame", deployer);
+
+
+};
+module.exports.tags = ["DiceGame"];
